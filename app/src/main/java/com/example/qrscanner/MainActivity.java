38fe453a918000cab6bb543d45d73f53834fa7ed
adminDRC <dennieschavez.09@gmail.com>
2024-06-17 20:41:46 +0800

    changes

--- app/src/main/java/com/example/qrscanner/MainActivity.java
+++ app/src/main/java/com/example/qrscanner/MainActivity.java
@@ -29,6 +29,7 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
